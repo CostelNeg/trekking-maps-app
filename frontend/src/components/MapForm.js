0cb2787b4164273086fcf7b31936afbb7e06c173
CostelNeg <costelnegura1705@gmail.com>
2024-10-09 21:18:16 +0200

    vediamo

---
+++ frontend/src/components/MapForm.js
@@ -0,0 +1,2 @@
+import React, {useState} from "react";
+import { addMap } from "../services/api";
---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import {login} from '../services/api.js';
+
+const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const result = await login ({email,password});
+        if(result.token) {
+            localStorage.setItem('token', result.token);
+        }
+    };
+
+    return(
+        <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    );
+}
+export default LoginPage;
