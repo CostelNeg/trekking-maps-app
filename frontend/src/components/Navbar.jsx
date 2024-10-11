67f528a648bfa593673468d40908d07ef6426a1f
CostelNeg <costelnegura1705@gmail.com>
2024-10-11 23:19:45 +0200

    2

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import '../HomeNav.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to='/'>Home</Link>
       <Link to='/login'>Login</Link>
+      <Link to='/'>Treaking Italia</Link>
       <Link to='/register'>Register</Link>
     </nav>
   );
